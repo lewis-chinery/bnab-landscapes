432e35a0e956c1b7673f35adfb319bf7ff50b6e6
lewis-chinery <lewis.chinery@dtc.ox.ac.uk>
2023-02-15 10:34:20 +0000

    Make pretty df with full H seq and edit distance

--- process_data.py
+++ process_data.py
@@ -49,6 +49,7 @@ def get_anarci_numbering():
     Hard coding to avoid anarci env dependency. Recreate using:
         raw_numbering = anarci.number(<seq>)
         [(str(res[0]) + res[1]).strip() for res, AA in raw_numbering[0] if AA != "-"]
+    Note - this is the same for noth 6261 and 9114
 
     :returns: list of imgt numbering
     '''
@@ -68,6 +69,7 @@ def get_positions_mutated_from_germline(ab, ignore_same_residues_as_paper=True):
     Get imgt positions that are mutated from germline for given ab
 
     :param ab: str with "9114" or "6261"
+    :param ignore_same_residues_as_paper: bool, ignore non-paratope mutations
     :returns: list of imgt numbers
     '''
 
@@ -96,6 +98,7 @@ def get_mutated_germline_vs_somatic_residues(ab, ignore_same_residues_as_paper=T
     Get AAs that differ between germline and somatic
 
     :param ab: str with "9114" or "6261"
+    :param ignore_same_residues_as_paper: bool, ignore non-paratope mutations
     :returns: two lists - first is AAs in germline, second is AAs in somatic ab
     '''
 
@@ -134,3 +137,106 @@ def load_raw_affinity_data(ab):
                                   dtype={'variant': str}))
     
     return df
+
+
+def get_columns_of_interest(ab):
+    '''
+    9114 and 6261 bind different Ags and have different column names
+
+    :param ab: str with "9114" or "6261"
+    :returns: list of present column names of interest
+    '''
+    if ab == "9114":
+        cols = ["variant", "h1_mean", "h3_mean", "fluB_mean", "som_mut"]
+    elif ab == "6261":
+        cols = ["variant", "h1_mean", "h9_mean", "som_mut"]
+    else:
+        raise ValueError("ab must be either '9114' or 6261")
+
+    return cols
+
+
+def binary_to_aa_str(binary, ab, ignore_same_residues_as_paper=True):
+    '''
+    Convert binary 10010... etc to matching amino acids
+
+    :param binary: str of 1s and 0s that indicate germline vs somatic residues
+    :param ab: str with "9114" or "6261"
+    :param ignore_same_residues_as_paper: bool, ignore non-paratope mutations
+    :returns: str of single letter amino acids for mutations only
+    '''
+
+    germ, som = get_mutated_germline_vs_somatic_residues(ab, ignore_same_residues_as_paper=True)
+
+    assert ignore_same_residues_as_paper is True, "Must ignore residues to match length of binary"
+    assert len(binary) == len(som), "lenght of binary str and list of somatic AAs must be the same"
+   
+
+    aa_str = ""
+    for bit, germ_aa, som_aa in zip([b for b in binary], germ, som):
+        if bit == "0":
+            aa_str += germ_aa
+        elif bit == "1":
+            aa_str += som_aa
+        else:
+            raise ValueError("binary should contain string ones and zeros only")
+        
+    return aa_str
+
+
+def binary_to_full_Hseq(binary, ab, ignore_same_residues_as_paper=True):
+    '''
+    Convert binary 10010... etc to matching amino acids and expand to whole H seq
+
+    :param binary: str of 1s and 0s that indicate germline vs somatic residues
+    :param ab: str with "9114" or "6261"
+    :param ignore_same_residues_as_paper: bool, ignore non-paratope mutations
+    :returns: str of single letter amino acids for whole H seq
+    '''
+    _, som = get_mutated_germline_vs_somatic_residues(ab, ignore_same_residues_as_paper=True)
+    mutated_positions = get_positions_mutated_from_germline(ab, ignore_same_residues_as_paper=True)
+
+    # note - ignoring any somatic mutations away from paratope by taking germline as base seq
+    full_germline = get_germline_sequence(ab)
+    full_numbering = get_anarci_numbering()
+
+    assert ignore_same_residues_as_paper is True, "Must ignore residues to match length of binary"
+    assert len(binary) == len(som), "lenght of binary str and list of somatic AAs must be the same"
+
+    full_H_seq = ""
+    for imgt, germ_aa in zip(full_numbering, full_germline):
+        if imgt in mutated_positions:
+            mut_idx = mutated_positions.index(imgt)
+            if binary[mut_idx] == "1":
+                full_H_seq += som[mut_idx]
+            else:
+                full_H_seq += germ_aa
+        else:
+            full_H_seq += germ_aa
+
+    return full_H_seq
+
+
+def load_processed_affinity_data(ab):
+    '''
+    Create df with only columns of interest, nicely ordered and named
+
+    :param ab: str with "9114" or "6261"
+    :returns: pretty df with affinity info
+    '''
+    raw_df = load_raw_affinity_data(ab)
+    processed_df = raw_df[get_columns_of_interest(ab)]
+    processed_df = processed_df.rename(columns={"variant": "binary_id"})
+
+    processed_df["str_aa_id"] = processed_df.apply(lambda row: binary_to_aa_str(row["binary_id"], ab), axis=1)
+    processed_df["H_seq"] = processed_df.apply(lambda row: binary_to_full_Hseq(row["binary_id"], ab), axis=1)
+
+    max_mutations = 16 if ab == "9114" else 11
+    processed_df["edit_distance"] = processed_df.apply(lambda row: max_mutations-row["som_mut"], axis=1)
+
+    unordered_cols = list(processed_df.columns)
+    ordered_cols = ["binary_id", "str_aa_id", "edit_distance", "H_seq"]
+    new_cols = ordered_cols + list(set(unordered_cols)-set(ordered_cols))
+    new_cols.remove("som_mut")
+
+    return processed_df[new_cols]
